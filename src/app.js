c5d079641f946d5da929bfc724bfd89241974d9d
George Aldington <georgealdington@gmail.com>
2022-12-07 14:09:21 +0000

    solved remainder problems

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
-const { add, subtract, multiply, divide } = require('./lib/numbers');
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
 const app = express();
 
@@ -86,6 +86,23 @@ app.post('/numbers/divide', (req, res) => {
     res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
   }
   res.status(200).json({ result: divide(n1, n2) });
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  const { a, b } = req.body;
+
+  const n1 = parseInt(a, 10);
+  const n2 = parseInt(b, 10);
+  if (n1 === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (n2 === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!a || !b) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(n1) && Number.isNaN(n2)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200).json({ result: remainder(n1, n2) });
 });
 
 module.exports = app;
