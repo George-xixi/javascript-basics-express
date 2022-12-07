6ee2000ce2b3d29c6956d5c31a56d1ea63d69fcb
George Aldington <georgealdington@gmail.com>
2022-12-07 11:52:11 +0000

    solved numbers katas

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
+const { add } = require('./lib/numbers');
+
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
@@ -32,5 +34,15 @@ app.get('/strings/first-characters/:string', (req, res) => {
     res.status(200).json({ result: firstCharacters(string, length) });
   }
   res.status(200).json({ result: firstCharacter(string) });
+});
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num1, 10);
+  const num2 = parseInt(req.params.num2, 10);
+
+  if (!isNaN(num1) && !isNaN(num2)) {
+    res.status(200).json({ result: add(num1, num2) });
+  }
+  res.status(400).json({ error: 'Parameters must be valid numbers.' });
 });
 module.exports = app;
